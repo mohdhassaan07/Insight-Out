2c09911cf503c8c004228f4c6c599768a12d5f92
mohdhassaan07 <mohdhassaan837@gmail.com>
2026-01-20 22:05:15 +0530

    signin signout with nextauth testes

--- app/(auth)/signout/page.tsx
+++ app/(auth)/signout/page.tsx
@@ -1,14 +1,19 @@
-import { getServerSession } from "next-auth"
+"use client";
+
 import { signOut } from "next-auth/react";
-export default async function signOutPage() {
-    const session = await getServerSession()
-    if (session) {
-        console.log("User is signed in:", session.user?.email);
-    }
-    return(
-        <div>
-            this is sign out page
-            
+
+export default function SignOutPage() {
+    
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen gap-4">
+            <h1 className="text-2xl font-bold">Sign Out</h1>
+            <p>Are you sure you want to sign out?</p>
+            <button
+                onClick={() => signOut({ callbackUrl: "/signin" })}
+                className="px-4 py-2 dark:bg-red-500 text-white border border-blue-500 rounded hover:bg-red-600 transition"
+            >
+                Confirm Sign Out
+            </button>
         </div>
-    )
+    );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,8 +2,9 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 export default async function dashboard() {
     const session = await getServerSession();
-    if (!session) redirect("/login");
+    if (!session) redirect("/signin");
+    
     return (
-        <div>This is dashboard</div>
+        <div>This is dashboard signed in as {session?.user?.email} </div>
     )
 }
