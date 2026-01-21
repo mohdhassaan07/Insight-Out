944e74454bb0b139c448b4079bbb00406befc3e9
mohdhassaan07 <mohdhassaan837@gmail.com>
2026-01-21 23:00:13 +0530

    starting with api rate limiting

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -4,38 +4,43 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 export async function POST(req: NextRequest) {
-    const body = await req.json();
-    const reqBody = z.object({
-        orgName: z.string().max(100),
-        name: z.string().min(3).max(100).regex(/^[a-zA-Z\s]+$/),
-        email: z.string().min(3).max(50).regex(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
-        password: z.string().min(3).max(30),
-    })
-    const parsedBody = reqBody.safeParse(body);
-    if (!parsedBody.success) {
-        return NextResponse.json({ error : "Invalid Input format" }, { status: 400 });
+    try {
+        const body = await req.json();
+        const reqBody = z.object({
+            orgName: z.string().max(100),
+            name: z.string().min(3).max(100).regex(/^[a-zA-Z\s]+$/),
+            email: z.string().min(3).max(50).regex(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
+            password: z.string().min(3).max(30),
+        })
+        const parsedBody = reqBody.safeParse(body);
+        if (!parsedBody.success) {
+            return NextResponse.json({ error: "Invalid Input format" }, { status: 400 });
+        }
+
+        const user = await prisma.user.findUnique({
+            where: { email: parsedBody.data.email }
+        });
+        if (user) {
+            return NextResponse.json({ error: "User already exists" }, { status: 400 });
+        }
+
+        const organization = await prisma.organization.create({
+            data: {
+                name: parsedBody.data.orgName,
+            }
+        });
+        const hashedPassword = await bcrypt.hash(parsedBody.data.password, 10);
+        const newuser = await prisma.user.create({
+            data: {
+                name: parsedBody.data.name,
+                email: parsedBody.data.email,
+                password: hashedPassword,
+                organizationId: organization.id
+            }
+        })
+        return NextResponse.json({ message: "User created successfully", user: newuser }, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
     }
 
-    const user = await prisma.user.findUnique({
-        where: { email: parsedBody.data.email }
-    });
-    if (user) {
-        return NextResponse.json({ error : "User already exists" }, { status: 400 });
-    }
-
-    const organization = await prisma.organization.create({
-        data : {
-            name: parsedBody.data.orgName,
-        }
-    });
-    const hashedPassword = await bcrypt.hash(parsedBody.data.password, 10);
-    const newuser = await prisma.user.create({
-        data : {
-            name: parsedBody.data.name,
-            email: parsedBody.data.email,
-            password: hashedPassword,
-            organizationId : organization.id
-        }
-    })
-    return NextResponse.json({ message : "User created successfully", user : newuser }, { status: 201 });
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import { getServerSession } from "next-auth";
 export default async function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        This is homepage
+    <div className="flex min-h-screen w-full items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex text-2xl font-mono min-h-screen min-w-40 flex-col items-center justify-center bg-white dark:bg-black sm:items-start">
+        Insight-Out – Your Feedbacks, Clearly Categorized
       </main>
     </div>
   );
